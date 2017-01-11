244f0ad10eefb7069a4ef26e350b066a9bcaf5a9
yxdshine <659777399@qq.com>
2017-01-12 00:29:48 +0800

    run susess

--- firstScalaProject/src/main/scala/com/yxd/bigdata/spark/core/YxdPvUv.scala
+++ firstScalaProject/src/main/scala/com/yxd/bigdata/spark/core/YxdPvUv.scala
@@ -12,26 +12,36 @@ import org.apache.spark.{SparkContext, SparkConf}
 object YxdPvUv {
 
   def main(args: Array[String]) {
-    //define cf
-    val scof = new SparkConf()
 
-    //define sparkcontext
-    val  sparkcontext =  SparkContext.getOrCreate(scof)
-    val  path = "/eventLogs/2016/12/21/FlumeData.1482391467667.log"
+    //val str ="192.168.25.128^A1483327400.959^A/hadoop.gif?en=e_e&ca=%E6%90%9C%E7%B4%A2%E4%BA%8B%E4%BB%B6%E6%B5%81&ac=%E8%AF%A6%E6%83%85%E9%A1%B5%E9%9D%A2%E6%B5%8F%E8%A7%88%E4%BA%8B%E4%BB%B6&ver=1&pl=website&sdk=js&u_ud=758EDFE1-7B54-419D-A020-3A0BB6FD81E2&u_mid=ibeifeng&u_sd=969FED4E-DFEC-4531-920F-6D22ED66BB3A&c_time=1483327399640&l=en-us&b_iev=Mozilla%2F5.0%20(X11%3B%20Linux%20x86_64)%20AppleWebKit%2F534.26%2B%20(KHTML%2C%20like%20Gecko)%20Version%2F5.0%20Safari%2F534.26%2B&b_rst=1280*768"
+   // val arr1= str.split("\\^A")
+    //define cf
+    val conf = new  SparkConf()
+      .setMaster("local[*]")
+      .setAppName("yxdtop")
+    val sparkcontext = new SparkContext(conf)
+    val  path = "/eventLogs/2017/01/02/FlumeData.1483327088054.log"
     val  pathRdd = sparkcontext.textFile(path)
     val  lengthFilterRdd = pathRdd.filter(line => line.length>0)
-    val  mapThreeRdd:RDD[(String,String,String)] = lengthFilterRdd.map(
+    val  arrthreeFilterRdd =  lengthFilterRdd.filter(line =>line.split("\\^A").size==3 )
+    val  mapThreeRdd:RDD[(String,String,String)] = arrthreeFilterRdd.map(
     line => {
-      val arr = line.split("^A")
+      print(line)
+      val arr = line.split("\\^A")
       var date:String=null
-      if(arr(1).length>0){
-        val dateTime = arr(1).split(".")(0)
-        val sdf:SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
-        date= sdf.format(new Date((dateTime.toLong)))
+      val  dateTimeSArr = arr(1).split("\\.")
+      var dateTime =""
+      if(dateTimeSArr!=null && dateTimeSArr.length>=0){
+        dateTime = dateTimeSArr(0)
+      }else{
+        dateTime = arr(1)
       }
+      val sdf:SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
+      date= sdf.format(new Date((dateTime.toLong)))
       (date,arr(0),arr(2))
+
     }
-    ).filter(tuple =>tuple._1!=null || tuple._1.length>0)
+    ).filter( tuple =>tuple._1!=null || tuple._1.length>0)
 
 
 
