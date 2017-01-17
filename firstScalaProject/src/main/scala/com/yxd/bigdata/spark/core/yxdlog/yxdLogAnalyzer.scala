e5fb5655bf9f92cb3be1b13f375c7bd65380549c
yxdshine <659777399@qq.com>
2017-01-17 23:27:25 +0800

    pro gb

--- firstScalaProject/src/main/scala/com/yxd/bigdata/spark/core/yxdlog/yxdLogAnalyzer.scala
+++ firstScalaProject/src/main/scala/com/yxd/bigdata/spark/core/yxdlog/yxdLogAnalyzer.scala
@@ -19,23 +19,27 @@ object yxdLogAnalyzer {
     val sparkContext =  SparkContext.getOrCreate(sparkCof)
 
     //filter data
-    val path = ""
+    val path = "/logtestdata/access.log"
     val pathRdd = sparkContext.textFile(path)
 
     val filterRdd = pathRdd.filter(line => line.length>0)//过滤长度
     .filter(line => yxdLogData.isVail(line))//过滤符合格式
     .map(line => yxdLogData.parseLogLine(line))//映射成我们想要的数组格式
 
+    //优化措施 多次使用的Rdd 缓存下
+    filterRdd.cache()
     /**
      * 需求一：计算整个响应时间的最大值和最小值，平均值
      */
     val durTimeRdd = filterRdd.map(line => line.durtime )
+    durTimeRdd.cache()
     val sum = durTimeRdd.sum()
     val max = durTimeRdd.max()
     val min = durTimeRdd.min()
     val size = durTimeRdd.count()
     val avg = 1.0*sum/size
 
+    durTimeRdd.unpersist()
     print(s" avg:${avg}, max:${max},min:${min}")
 
 
@@ -52,7 +56,11 @@ object yxdLogAnalyzer {
     /**
      * 计算IP唯一数 并且大于2
      */
-    val ipRdd = filterRdd.map(line => (line.ip,1))
+    val blackIP = Array("200-55-104-193.dsl.prima.net.ar", "10.0.0.153", "208-38-57-205.ip.cal.radiant.net")
+    // 由于集合比较大，将集合的内容广播出去
+    val broadCastIP = sparkContext.broadcast(blackIP)
+    val ipRdd = filterRdd.filter(line => !broadCastIP.value.contains(line.ip))
+      .map(line => (line.ip,1))
     .reduceByKey(_+_).filter(tuple => tuple._2>2)
 
     print(s"""iprdd:${ipRdd.collect().mkString(",")}""")
@@ -65,7 +73,7 @@ object yxdLogAnalyzer {
       .reduceByKey(_+_).top(3)(yxdDefineSort.TupleSort)
 
     print(s"""resCode:${resCodeTopRdd.mkString(",")}""")
-
+    filterRdd.unpersist()
     //stop sparkcof
     sparkContext.stop()
   }
